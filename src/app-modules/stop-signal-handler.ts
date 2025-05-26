04a2943210cda9eb7316a5da5c2e440d550d0d38
Dave van Soest <tbknl@davevansoest.com>
2025-05-26 22:33:06 +0200

    feat: add control-server and stop-signal-handler built-in app-modules

---
+++ src/app-modules/stop-signal-handler.ts
@@ -0,0 +1,34 @@
+
+interface Logger {
+	info: (msg: string) => void;
+}
+
+export const makeStopSignalHandler = ({ logger }: { logger?: Logger }) => ({
+	initialize: async (
+		_cfg: null,
+		{ lifecycle }: {
+			lifecycle: {
+				stop: () => void,
+				status: () => {
+					inStoppablePhase: boolean;
+					phase: string;
+				}
+			}
+		}
+	) => {
+		(['SIGHUP', 'SIGINT', 'SIGTERM'] as const).forEach((sig) => {
+			process.on(sig, () => {
+				const status = lifecycle.status();
+				if (status.inStoppablePhase) {
+					logger?.info(`Stopping after receiving signal ${sig}.`);
+					lifecycle.stop();
+				}
+				else {
+					logger?.info(`Ignoring received signal ${sig}, because lifecycle is in the non-stoppable phase ${status.phase}.`);
+				}
+			});
+		});
+		return { instance: {} };
+	},
+	options: { orderedFinalization: true },
+} as const);
