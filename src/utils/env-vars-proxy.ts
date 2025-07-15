34ac1f7ec51dff57f192127dcb36934bc5a4537a
Dave van Soest <tbknl@davevansoest.com>
2025-07-15 08:46:27 +0200

    Tests for env-vars proxy.

--- src/utils/env-vars-proxy.ts
+++ src/utils/env-vars-proxy.ts
@@ -1,67 +1,72 @@
 export type EnvVars = { [key: string]: string | undefined };
 
 type EnvAccess =
-  | {
-      readonly type: 'read';
-      readonly present: true;
-      readonly key: string;
-      readonly value: unknown;
-    }
-  | {
-      readonly type: 'read';
-      readonly present: false;
-      readonly key: string;
-    }
-  | {
-      readonly type: 'check';
-      readonly present: boolean;
-      readonly key: string;
-    };
+	| {
+		readonly type: 'read';
+		readonly present: true;
+		readonly key: string;
+		readonly value: unknown;
+	}
+	| {
+		readonly type: 'read';
+		readonly present: false;
+		readonly key: string;
+	}
+	| {
+		readonly type: 'check';
+		readonly present: boolean;
+		readonly key: string;
+	};
 
-export const makeEnvProxy = (envVars: EnvVars, { logger }: { logger: { error: (msg: string) => void } }) => {
-  let locked = false;
+export const makeEnvProxy = (envVars: EnvVars, options?: { onLockedAccess?: ({ key }: { key: string }) => (boolean | void) }) => {
+	let locked = false;
 
-  const checkLock = (key: string) => {
-    if (locked) {
-      logger.error(`Unallowed access to env var '${key}' outside configuration phase.`);
-    }
-  };
+	const checkLock = (key: string) => {
+		if (locked) {
+			if (!options?.onLockedAccess || !options?.onLockedAccess({ key })) {
+				throw new Error(`Unallowed access to env var '${key}' outside configuration phase.`);
+			}
+		}
+	};
 
-  const envAccessLog: EnvAccess[] = [];
+	const envAccessLog: EnvAccess[] = [];
 
-  const envVarsProxy = new Proxy(envVars, {
-    get: (target, prop) => {
-      const strProp = String(prop);
-      checkLock(strProp);
-      const value = target[strProp];
-      const present = target.hasOwnProperty(prop);
-      envAccessLog.push({
-        type: 'read',
-        key: strProp,
-        ...(present ? ({ present: true, value } as const) : ({ present: false } as const)),
-      } as const);
-      return value;
-    },
-    has: (target, prop) => {
-      const strProp = String(prop);
-      checkLock(strProp);
-      const present = target.hasOwnProperty(strProp);
-      envAccessLog.push({
-        type: 'check',
-        key: strProp,
-        present,
-      });
-      return present;
-    },
-  });
+	const envVarsProxy = new Proxy(envVars, {
+		get: (target, prop) => {
+			const strProp = String(prop);
+			checkLock(strProp);
+			const value = target[strProp];
+			const present = target.hasOwnProperty(prop);
+			envAccessLog.push({
+				type: 'read',
+				key: strProp,
+				...(present ? ({ present: true, value } as const) : ({ present: false } as const)),
+			} as const);
+			return value;
+		},
+		has: (target, prop) => {
+			const strProp = String(prop);
+			checkLock(strProp);
+			const present = target.hasOwnProperty(strProp);
+			envAccessLog.push({
+				type: 'check',
+				key: strProp,
+				present,
+			});
+			return present;
+		},
+		set: (_target, prop) => {
+			throw new Error(`Cannot set property ${prop.toString()} on read-only env vars proxy.`);
+		},
+	});
 
-  return {
-    vars: envVarsProxy,
-    lock() {
-      locked = true;
-    },
-    accessLog() {
-      return envAccessLog;
-    },
-  };
+	return {
+		vars: envVarsProxy,
+		lock() {
+			locked = true;
+		},
+		accessLog() {
+			return envAccessLog;
+		},
+	};
 };
---
+++ test/utils/env-vars-proxy.test.ts
@@ -0,0 +1,134 @@
+import { describe, it, expect, vi } from 'vitest';
+import { makeEnvProxy } from 'modstack/utils/env-vars-proxy';
+
+describe('env proxy', () => {
+	it('allows read access to the underlying env data', () => {
+		const envVars = { TEST_VAR: 'testValue' };
+		const envProxy = makeEnvProxy(envVars);
+		expect(envProxy.vars.TEST_VAR).toEqual(envVars.TEST_VAR);
+	});
+
+	it('allows check access to the underlying env data', () => {
+		const testKey = 'TEST_VAR';
+		const envVars = { [testKey]: 'testValue' };
+		const envProxy = makeEnvProxy(envVars);
+		expect(testKey in envProxy.vars).toBe(true);
+		expect('UNEXISTING_VAR' in envProxy.vars).toBe(false);
+	});
+
+	it('does not allow write operations', () => {
+		const envProxy = makeEnvProxy({});
+		expect(() => { envProxy.vars['TEST_KEY'] = 'value'; }).toThrow();
+	});
+
+	describe('access logging', () => {
+		it('starts with an empty access log', () => {
+			const envProxy = makeEnvProxy({});
+			expect(envProxy.accessLog()).toEqual([]);
+		});
+
+		it('logs access when reading a property that is present', () => {
+			const key = 'X';
+			const value = 'env-value';
+			const envProxy = makeEnvProxy({ [key]: value });
+
+			envProxy.vars[key];
+
+			expect(envProxy.accessLog()).toEqual([
+				{ type: 'read', key, present: true, value },
+			]);
+		});
+
+		it('logs access when reading a property that is not present', () => {
+			const key = 'X';
+			const envProxy = makeEnvProxy({});
+
+			envProxy.vars[key];
+
+			expect(envProxy.accessLog()).toEqual([
+				{ type: 'read', key, present: false },
+			]);
+		});
+
+		it('logs access when checking a property that is present', () => {
+			const key = 'X';
+			const value = 'env-value';
+			const envProxy = makeEnvProxy({ [key]: value });
+
+			key in envProxy.vars;
+
+			expect(envProxy.accessLog()).toEqual([
+				{ type: 'check', key, present: true },
+			]);
+		});
+
+		it('logs access when checking a property that is not present', () => {
+			const key = 'X';
+			const envProxy = makeEnvProxy({});
+
+			key in envProxy.vars;
+
+			expect(envProxy.accessLog()).toEqual([
+				{ type: 'check', key, present: false },
+			]);
+		});
+
+		it('keeps a log of all accesses', () => {
+			const key1 = 'X';
+			const key2 = 'Y';
+			const key3 = 'Z';
+			const value = 'env-value';
+			const envProxy = makeEnvProxy({ [key1]: value, [key2]: value });
+
+			envProxy.vars[key1];
+			key2 in envProxy.vars;
+			envProxy.vars[key2];
+			key3 in envProxy.vars;
+
+			expect(envProxy.accessLog()).toEqual([
+				{ type: 'read', key: key1, present: true, value },
+				{ type: 'check', key: key2, present: true },
+				{ type: 'read', key: key2, present: true, value },
+				{ type: 'check', key: key3, present: false },
+			]);
+		});
+
+	});
+
+	describe('locking', () => {
+		it('by default throws when trying to access env vars after locking', () => {
+			const envProxy = makeEnvProxy({});
+			expect(envProxy.vars['X']).toBeUndefined();
+			envProxy.lock();
+			expect(() => envProxy.vars['X']).toThrow();
+		});
+
+		it('allows to access env vars after locking when onLockedAccess returns true', () => {
+			const key = 'X';
+			const onLockedAccess = vi.fn(({}: { key: string }) => true);
+			const value = 'env-value';
+			const envProxy = makeEnvProxy({ [key]: value }, { onLockedAccess });
+
+			expect(envProxy.vars[key]).toEqual(value);
+			expect(onLockedAccess).toHaveBeenCalledTimes(0);
+
+			envProxy.lock();
+			expect(envProxy.vars[key]).toEqual(value);
+			expect(onLockedAccess).toHaveBeenCalledExactlyOnceWith({ key });
+		});
+
+		it('throws when trying to access env vars after locking and onLockedAccess returns false', () => {
+			const key = 'X';
+			const onLockedAccess = vi.fn(({}: { key: string }) => false);
+			const value = 'env-value';
+			const envProxy = makeEnvProxy({ [key]: value }, { onLockedAccess });
+
+			expect(envProxy.vars[key]).toEqual(value);
+			expect(onLockedAccess).toHaveBeenCalledTimes(0);
+
+			envProxy.lock();
+			expect(() => envProxy.vars[key]).toThrow();
+			expect(onLockedAccess).toHaveBeenCalledExactlyOnceWith({ key });
+		});
+	});
+});
