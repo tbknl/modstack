e2f0eaf86153382317b383a12001aafc1b312f49
Dave van Soest <tbknl@davevansoest.com>
2025-09-01 16:00:32 +0200

    Improved app-module-compatibility types.

--- src/utils/app-module-compatibility.ts
+++ src/utils/app-module-compatibility.ts
@@ -1,19 +1,18 @@
+class FailureToInfer<_Subject extends string> {}
+
 // Infer the instance type of an app-module:
 export type AppModuleInstance<AppModule> = AppModule extends {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly initialize: (...args: any[]) => Promise<{ instance: infer Inst }>;
+	readonly initialize: (...args: any[]) => Promise<{ instance: infer Inst }>;
 }
-  ? Inst
-  : never;
+	? Inst
+	: FailureToInfer<'AppModuleInstance'>;
 
 // Infer the dependencies type of an app-module:
 export type AppModuleDependencies<AppModule> = AppModule extends {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly initialize: (cfg: any, deps: infer Deps) => Promise<{ instance: unknown }>;
+	readonly initialize: (cfg: any, deps: infer Deps) => Promise<{ instance: unknown }>;
 }
-  ? Deps
-  : never;
+	? Deps
+	: FailureToInfer<'AppModuleDependencies'>;
 
 // Checks whether second type argument (_U) is compatible with the first type argument (T).
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const isCompatible = <T, _U extends T>() => true;
+export const isCompatible = <T extends {}, _U extends T>() => true;
